9cf692016a1818b6723194593bb6d6076765698c
Jun Young Lee <legshort@gmail.com>
2017-11-15 23:00:16 +0900

    test(codecov): codecov 테스트 코드 추가

--- django_demo/users/tests.py
+++ django_demo/users/tests.py
@@ -4,3 +4,6 @@ from rest_framework.test import APITestCase
 class UserTestCase(APITestCase):
     def test(self):
         self.assertTrue(True)
+
+    def test_user(self):
+        self.assertTrue(True)
